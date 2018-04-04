6aba63682322e6807b4dc78e7a5dd37af71f5454
Bethany Powell <beth@powellcastle.com>
2018-04-03 23:06:12 -0500

    Checkpoint6 v2

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 
+
 class Album extends Component {
   constructor(props) {
     super(props);
 
     const album = albumData.find( album => {
-         return album.slug === this.props.match.params.slug
-       });
+       return album.slug === this.props.match.params.slug
+     });
 
-       this.state = {
-         album: album
-       };
+     this.state = {
+       album: album
+     };
      }
 
   render() {
     return (
       <section className="album">
       <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+           <img id="album-cover-art" src={this.state.album.albumCover} alt={''} />
            <div className="album-details">
            <h1 id="album-title">{this.state.album.title}</h1>
            <h2 className="artist">{this.state.album.artist}</h2>
