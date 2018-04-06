493ed1c848f81cadd8b37b84760e693eb094bf59
Bethany Powell <beth@powellcastle.com>
2018-04-05 22:17:09 -0500

    Checkpoint6 v2

--- src/components/Album.js
+++ src/components/Album.js
@@ -18,22 +18,27 @@ class Album extends Component {
   render() {
     return (
       <section className="album">
-      <section id="album-info">
+        <section id="album-info">
            <img id="album-cover-art" alt={''} src={this.state.album.albumCover || ''}  />
            <div className="album-details">
              <h1 id="album-title">{this.state.album.title}</h1>
              <h2 className="artist">{this.state.album.artist}</h2>
              <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-           <table id="song-list">
-           <colgroup>
-             <col id="song-number-column" />
-             <col id="song-title-column" />
-             <col id="song-duration-column" />
-           </colgroup>
-           <tbody>
-           </tbody>
-         </table>
+            </div>
+
+             <div className="song-list">
+               <table id="song-list">
+                 <tbody>
+                  {this.state.album.songs.map( (album, index) =>
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{album.title}</td>
+                      <td>{album.duration}</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
          </section>
       </section>
     );
