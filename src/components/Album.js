609e0f80ae5ea6c00d64454d1ac4eedbdc2e6418
Bethany Powell <beth@powellcastle.com>
2018-04-09 21:21:36 -0500

    Checkpoint8 v2

--- src/components/Album.js
+++ src/components/Album.js
@@ -46,41 +46,54 @@ class Album extends Component {
       }
     }
 
-  render() {
-    return (
-      <section className="album">
-        <section id="album-info">
-           <img id="album-cover-art" alt={''} src={this.state.album.albumCover}  />
-           <div className="album-details">
-             <h1 id="album-title">{this.state.album.title}</h1>
-             <h2 className="artist">{this.state.album.artist}</h2>
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
+    handlePrevClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play(newSong);
+    }
 
+    render() {
+       return (
+         <section className="album">
+           <section id="album-info">
+             <img id="album-cover-art" src={this.state.album.albumCover} alt=""/>
+             <div className="album-details">
+               <h1 id="album-title">{this.state.album.title}</h1>
+               <h2 className="artist">{this.state.album.artist}</h2>
+               <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
+             </div>
+           </section>
            <table id="song-list">
+             <colgroup>
+               <col id="song-number-column" />
+               <col id="song-title-column" />
+               <col id="song-duration-column" />
+             </colgroup>
              <tbody>
-             {this.state.album.songs.map( (song, index) =>
-               <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-                 <td className="song-actions">
-                   <button>
-                     <span className="song-number">{index+1}</span>
-                     <span className="ion-play"></span>
-                     <span className="ion-pause"></span>
-                   </button>
-                 </td>
-                 <td className="song-title">{song.title}</td>
-                 <td className="song-duration">{song.duration}</td>
-               </tr>
+               {this.state.album.songs.map( (song, index) =>
+                 <tr className="song" key={index} onClick={ () => this.handleSongClick(song) } >
+                   <td className="song-actions">
+                     <button>
+                       <span className="song-number">{index+1}</span>
+                       <span className="ion-play"></span>
+                       <span className="ion-pause"></span>
+                     </button>
+                   </td>
+                   <td className="song-title">{song.title}</td>
+                   <td className="song-duration">{song.duration}</td>
+                 </tr>
                )}
-              </tbody>
-            </table>
-              <PlayerBar
-                isPlaying={this.state.isPlaying}
-                currentSong={this.state.currentSong}
-                handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-              />
+             </tbody>
+           </table>
+            <PlayerBar
+              isPlaying={this.state.isPlaying}
+              currentSong={this.state.currentSong}
+              handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+              handlePrevClick={() => this.handlePrevClick()}
+             />
          </section>
-      </section>
     );
   }
 }
