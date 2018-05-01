90fce433786a54ff6aafd458ff3a0a411e91efd8
Bethany Powell <beth@powellcastle.com>
2018-04-30 20:35:25 -0500

    Updated Library styling

--- src/components/Library.js
+++ src/components/Library.js
@@ -15,10 +15,10 @@ class Library extends Component {
       {
           this.state.albums.map( (album, index) =>
             <Link to={`/album/${album.slug}`} key={index}>
-            <img src={album.albumCover} alt={album.title} />
-            <div>{album.title}</div>
-            <div>{album.artist}</div>
-            <div>{album.songs.length} songs</div>
+            <img className="album-cover album-library" src={album.albumCover} alt={album.title} />
+            <div className="album-title column-third">{album.title}</div>
+            <div className="album-artist column-third">{album.artist}</div>
+            <div className="number-songs column-third">{album.songs.length} songs</div>
             </Link>
           )
         }
